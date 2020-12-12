58cfb743ccc840137b6f4cce896c3e2e4b5a93aa
BotGua <74773543+BotGua@users.noreply.github.com>
2020-12-12 21:59:03 +0700

    Update index.js

--- index.js
+++ index.js
@@ -19,12 +19,12 @@ const arrayBulan = ['Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Jul
 const bulan = arrayBulan[moment().format('MM') - 1]
 
 const config = {
-    A187: '👾AR15BOT👾',
-    instagram: 'https://instagram.com/_sadboy.ig',
-    nomer: 'wa.me/6285722553839',
+    A187: '🖤Akb4r™🖤',
+    instagram: 'https://instagram.com/kamaadoo_tanjiroo',
+    nomer: 'wa.me/+6281368965962',
     aktif: '24 JAM',
-    youtube: 'https://youtube.com/channel/UCGYLWtyT9IADYNUiK0uZiGg',
-    whatsapp: 'https://chat.whatsapp.com/DSSHmG2KjKJLoFp9B9mkVs',
+    youtube: 'Belum buat',
+    whatsapp: 'https://chat.whatsapp.com/K4vJLX34mvqIDS7cd05Gfi',
     tanggal: `TANGGAL: ${moment().format('DD')} ${bulan} ${moment().format('YYYY')}`,
     waktu: time
 }
@@ -121,11 +121,11 @@ fs.existsSync('./session.json') && client.loadAuthInfo('./session.json')
 
 client.connect();
 
-// client.on('user-presence-update', json => console.log(json.id + ' presence is => ' + json.type)) || console.log(`${time}: Bot by ig:@aditiaalfians`)
+// client.on('user-presence-update', json => console.log(json.id + ' presence is => ' + json.type)) || console.log(`${time}: Bot by ig:@kamaadoo_,tanjiroo`)
 
 client.on('message-status-update', json => {
    const participant = json.participant ? ' (' + json.participant + ')' : ''
-   console.log(`[ ${time} ] => bot by ig:@_sadboy.ig`)
+   console.log(`[ ${time} ] => bot by ig:@kamaadoo_tanjiroo`)
 })
 
 client.on('message-new', async (m) => {
